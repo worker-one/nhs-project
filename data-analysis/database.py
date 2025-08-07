5e1845b15f6777f4a22f1f6c6956cbadd6f2341f
Konstantin Verner <konst.verner@gmail.com>
2025-08-07 16:20:19 +0200

    wip

--- data-analysis/database.py
+++ data-analysis/database.py
@@ -1,5 +1,5 @@
 import os
 from sqlalchemy import create_engine
 
-db_path = os.path.join(os.path.dirname(__file__), '../nhs_db.db')
+db_path = os.path.join(os.path.dirname(__file__), '../nhs.db')
 engine = create_engine(f'sqlite:///{db_path}')
